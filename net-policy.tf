5e5173237cfbb7518f332afadeb9e2f15be9a0d4
anaslahham <anaslahham@gmail.com>
2022-11-08 15:15:58 +0100

    Update net-policy.tf

--- net-policy.tf
+++ net-policy.tf
@@ -5,21 +5,23 @@ resource "kubernetes_network_policy_v1" "test-customer" {
   }
 
   spec {
-    policy_types = ["Ingress"] 
     pod_selector {
-      app = "test-nginx"
+      match_expressions {
+        key      = "app"
+        operator = "In"
+        values   = ["test-nginx"]
     }
-
+}
     ingress {
       ports {
         port     = "80"
         protocol = "TCP"
       }
- 
+
 
       }
 
-      
+    policy_types = ["Ingress"]
     }
 
      }
