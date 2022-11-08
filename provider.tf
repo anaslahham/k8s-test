fce2b21ce6ee5504ac1b489ce878aae15204a59f
anaslahham <anaslahham@gmail.com>
2022-11-08 11:18:53 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "/home/ubuntu/terraform-k8s/config"
+  config_context = "kubernetes-admin@kubernetes"
+}
+~
