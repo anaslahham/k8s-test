7eb43ad9fd43f01568ea373f92dd6ceda36fd5d9
anaslahham <anaslahham@gmail.com>
2022-11-08 15:04:15 +0100

    Create net-policy.tf

---
+++ net-policy.tf
@@ -0,0 +1,27 @@
+resource "kubernetes_network_policy_v1" "test-customer" {
+  metadata {
+    name      = "test-customer"
+    namespace = "test-customer"
+  }
+
+  spec {
+    pod_selector {
+     app = "test-nginx"
+    }
+
+    ingress {
+      ports {
+        port     = "80"
+        protocol = "TCP"
+      }
+ 
+
+      }
+
+
+    }
+
+
+    policy_types = ["Ingress"]
+  }
+}
